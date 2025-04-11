2de1245447a5bf2cd26fa6b2d761705814137a39
Law44 <davidddd4444@gmail.com>
2025-04-11 15:04:21 +0200

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-<<<<<<< HEAD
-        System.out.printf("Hello and NEIL CONFLICTO!");
+        System.out.printf("Hello world examen");
     }
 }
